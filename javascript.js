77cf3d2b0be830ab9a0e1def160da602e50ebb07
¨Smurf¨ <65689263+dwithers85@users.noreply.github.com>
2026-06-23 00:32:37 -0700

    add getHumanChoice function, initialize score

--- javascript.js
+++ javascript.js
@@ -3,13 +3,22 @@ console.log("Hello World");
 function getComputerChoice(){
     let x = Math.floor(Math.random()*3);
     if (x === 0){
-        return "Rock";
+        return "rock";
     
     }else if(x===1){
-        return "Paper";
+        return "paper";
     }else if(x===2){
-        return "Scissors";
+        return "scissors";
     }
-}
+}//getComputer Choice
+
+function getHumanChoice(){
+    let result = prompt("Enter rock, paper, or scissors");
+    return result.toLowerCase();
+}//getHumanChoice
+
+let humanScore = 0;
+let computerScore = 0;
 
 console.log(getComputerChoice());
+console.log(getHumanChoice());
